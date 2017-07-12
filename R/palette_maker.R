fba8bcd8a4556b02087df05e88e0c990765d55c0
Victor Lei <vlei@legendary.com>
2017-07-11 20:03:26 -0400

    Updated to use any valid color space in the colorspaces package. Also sorted color outputs and adjusted/exposed the kmeans parameters to get better results.

--- R/palette_maker.R
+++ R/palette_maker.R
@@ -26,3 +26,51 @@ palette_maker <- function(image_path = NA, number_of_colors = 20){
   show_col(colours_vector)
   return(colours_vector)
 }
+
+
+#' Make a palette from your image in a particular color space
+#'
+#' Processes a custom jpeg image producing a palett with a
+#'   user-defined number of colours. This will allow any color space available
+#'   in the `colorspace` package. There may be conversion issues if the selected
+#'   color space does not have a valid representation of the all RGB values in
+#'   the picture.
+#'
+#' @param image_path Relative path to the custom image, including .jpeg
+#'   extension
+#' @param number_of_colors Number of different colors desired for the resulting
+#'   palette
+#' @param use_LAB Whether to use the CIELAB space instead of the default RGB
+#' @param colspace A valid color class from the `colorspace` package.
+#' @inheritParams stats::kmeans
+#' @author Victor Lei (based on Andrea Cirillo's original code)
+#' @examples
+#' palette_maker_cs('data/nascita_venere.jpg', number_of_colors = 20, colspace = "LAB", iter.max = 100)
+#' @import colorspace
+#' @export
+palette_maker_cs <- function(image_path = NA, number_of_colors = 20, colspace = "RGB",
+                          iter.max = 50, nstart = 1, plot_colors = TRUE) {
+  if( !(colspace %in% c('RGB', 'LAB', 'LUV', 'XYZ', 'HLS', 'HSV', 'polarLUV', 'polarLAB', 'sRGB') ) ) {
+    stop("Color space ", colspace, " not supported")
+  }
+  if (is.na(image_path)) {
+    stop("You must provide a jpg image to build your palette from")
+  }
+  painting <- jpeg::readJPEG(image_path)
+  painting_raw_rgb_data <- as.matrix(data.frame(R = as.vector(painting[,, 1]),
+                             G = as.vector(painting[,, 2]),
+                             B = as.vector(painting[,, 3])))
+
+  painting_rgb <- as(colorspace::RGB(painting_raw_rgb_data), colspace)
+
+  k_means <- stats::kmeans(colorspace::coords(painting_rgb), centers = number_of_colors,
+                           iter.max = iter.max, nstart = nstart)
+
+  colours_k <- colorspace::hex(do.call(colspace, list(k_means$centers)))
+
+  colors_vector <- sort(colours_k, decreasing = TRUE)
+
+  if(plot_colors) scales::show_col(colors_vector)
+
+  return(colors_vector)
+}
